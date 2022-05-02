3990f1aa5cfbac5d1854043a6716d79877ff3b2a
QR <1826255833@qq.com>
2022-05-02 15:45:10 +0800

    update note

--- gateway/src/main/java/qr/program/microservice/gateway/AuthorizeFilter.java
+++ gateway/src/main/java/qr/program/microservice/gateway/AuthorizeFilter.java
@@ -3,6 +3,7 @@ package qr.program.microservice.gateway;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
@@ -10,18 +11,22 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-// @Order(-1)
+/**
+ * 全局过滤器，可以自定义逻辑
+ */
+@Order(-1) // 设置过滤器的拦截顺序，越小拦截器越前
 @Component
-public class AuthorizeFilter implements GlobalFilter, Ordered {
+public class AuthorizeFilter implements GlobalFilter {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         // 1.获取请求参数
         ServerHttpRequest request = exchange.getRequest();
         MultiValueMap<String, String> params = request.getQueryParams();
-        // 2.获取参数中的 authorization 参数
+        // 2.获取参数
         String auth = params.getFirst("authorization");
+        String author = params.getFirst("author");
         // 3.判断参数值是否等于 admin
-        if ("admin".equals(auth)) {
+        if ("admin".equals(auth) || "qr".equals(author)) {
             // 4.是，放行
             return chain.filter(exchange);
         }
@@ -30,10 +35,5 @@ public class AuthorizeFilter implements GlobalFilter, Ordered {
         exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
         // 5.2.拦截请求
         return exchange.getResponse().setComplete();
-    }
-
-    @Override
-    public int getOrder() {
-        return -1;
     }
 }
